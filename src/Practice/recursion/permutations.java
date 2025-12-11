525196ee2a4c1a141fa5376bef6b0e44348cfa97
Sai Varshith Kothwali <kothwalisaivarshith@gmail.com>
2025-12-11 15:10:10 +0530

    Permutations

--- src/Practice/recursion/permutations.java
+++ src/Practice/recursion/permutations.java
@@ -4,29 +4,29 @@ import java.util.ArrayList;
 
 public class permutations
 {
-    static ArrayList<String> list=new ArrayList<>();
+
     public static void main(String[] args)
     {
-        String str="abc";
-        permu("",str);
-        System.out.println(list);
+        permu("","abc");
     }
     public static void permu(String p,String up)
     {
         if(up.isEmpty())
         {
-           list.add(p);
+            System.out.println(p);
             return;
         }
-
         char ch=up.charAt(0);
+
         for(int i=0;i<=p.length();i++)
         {
-            String f=p.substring(0,i);
-            String l=p.substring(i);
-            permu(f+ch+l,up.substring(1));
+            String first=p.substring(0,i);
+            String last=p.substring(i,p.length());
 
+            permu(first+ch+last,up.substring(1));
         }
+
+
     }
 }
 
